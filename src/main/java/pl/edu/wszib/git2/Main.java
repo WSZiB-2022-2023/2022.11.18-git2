3d7fae8e60c505b2ae05a2f1f5ab80c005918689
Mateusz <mat.bereda@gmail.com>
2022-11-18 12:45:38 +0100

    cos w nowym branchu

--- src/main/java/pl/edu/wszib/git2/Main.java
+++ src/main/java/pl/edu/wszib/git2/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("nowa rzecz");
 
         System.out.println("kolejna nowa rzecz");
+
+        System.out.println("nowa rzecz w nowym branchu !!");
     }
 }
